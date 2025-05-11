aafce6f19e4d848799899089852c30b998127a56
Artur Babii <alfred.dc.alpha@gmail.com>
2025-05-11 18:23:48 +0200

    cleaning comments

--- rx/main.py
+++ rx/main.py
@@ -2,19 +2,19 @@ from machine import Pin, SPI
 from time import sleep, ticks_ms, ticks_diff
 from nrf24l01 import NRF24L01
 
-# Піни для SPI
+# Pins for SPI
 spi = SPI(0, sck=Pin(18), mosi=Pin(19), miso=Pin(16))
 csn = Pin(17, Pin.OUT)
 ce = Pin(20, Pin.OUT)
 led = Pin(25, Pin.OUT)
 
-# Ініціалізація NRF24L01
+# Initialization of the NRF24L01 module
 nrf = NRF24L01(spi, csn, ce, channel=100, payload_size=5)
 nrf.open_rx_pipe(1, b'1Node')
-# Встановити 2 Mbps
+# Set speed 2 Mbps
 nrf.reg_write(0x06, 0b00001010)
 
-# Вимкнути автопідтвердження (ACK)
+# Disable ACK
 nrf.reg_write(0x01, 0b00000000)
 
 nrf.start_listening()
@@ -29,7 +29,7 @@ while True:
         buf = nrf.recv()
         packet_count_per_sec += 1
       
-    # Перевірка кожну секунду
+    # Every second print packets per second
     if ticks_diff(ticks_ms(), last_time) >= 1000:
         print("Packets per sec:", packet_count_per_sec)
         packet_count_per_sec = 0
